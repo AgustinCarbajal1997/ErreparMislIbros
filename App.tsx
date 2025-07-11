16de170ed13d2857e13c82972c34d4c2608275e1
Agustin Carbajal <magustincarbajal97@gmail.com>
2025-07-11 11:48:36 -0300

    bootsplash

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import DrawerNavigation from './src/navigation/navigation';
 import AuthStack from './src/navigation/stacks/AuthStack';
 import {AuthProvider, useAuth} from './src/context/AuthContext';
+import BootSplash from 'react-native-bootsplash';
 
 const AppNavigator = () => {
   const {isAuthenticated} = useAuth();
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
 
+    init().finally(async () => {
+      await BootSplash.hide({fade: true});
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
   return (
     <NavigationContainer>
       {isAuthenticated ? <DrawerNavigation /> : <AuthStack />}
